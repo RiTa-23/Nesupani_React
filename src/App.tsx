956b8dd4bee3a4aea3b388897461cc70ef658eb3
RiTa-23 <rita.tifrt@gmail.com>
2025-05-12 15:01:07 +0900

    add:mediapipeで手のランドマーク描写

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import GamePage from './pages/GamePage';
+import HandsTest from './pages/HandsTest';
+
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/game" element={<GamePage />} />
+        <Route path="/test" element={<HandsTest />} />
       </Routes>
     </Router>
   );
