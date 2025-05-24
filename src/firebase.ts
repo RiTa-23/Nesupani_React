0a0af740ea95086d90c279e887d6f15203ad4f63
RiTa-23 <rita.tifrt@gmail.com>
2025-05-24 10:15:24 +0900

    add:firestore導入

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  // ここにFirebaseコンソールで取得した設定を記載
+  apiKey: "AIzaSyDIddFxNb0l6WaQKa0AlLDZw43yMLRsRTU",
+  authDomain: "nesugoshipanic.firebaseapp.com",
+  projectId: "nesugoshipanic",
+  storageBucket: "nesugoshipanic.firebasestorage.app",
+  messagingSenderId: "122002042444",
+  appId: "1:122002042444:web:28968c35f4886b37638ee9"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
